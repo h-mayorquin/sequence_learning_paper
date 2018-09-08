6d29e0c5fa58430af68d4b2f81b2fe77927d2b62
h-mayorquin <h.mayorquin@gmail.com>
2018-09-08 20:47:34 +0200

    Annoying changes

--- play.py
+++ play.py
@@ -14,19 +14,56 @@ epsilon = 10e-80
 
 np.seterr(over='raise')
 
-def covariance(t1, t2, tau_s, sigma_in):
-    factor = tau_s * sigma_in * 0.5
-    diff = np.abs(t1 - t2)
-    addition = t1 + t2
+strict_maximum = True
 
-    return factor * (np.exp(-diff / tau_s) + np.exp(-addition / tau_s))
-
+g_a = 1.0
+g_I = 2.0
+tau_a = 0.250
+G = 1.0
+sigma_out = 0.0
 tau_s = 0.010
+tau_z_pre = 0.025
+tau_z_post = 0.005
+
+hypercolumns = 10
+minicolumns = 40
+n_patterns = minicolumns
+representation_overlap = 1.0
+sequence_overlap = 0.5
+
+
+# Training protocol
+ws = 1.0
+wn = 0.5
+wb = -0.5
+alpha = 0.5
+alpha_back = 3.0
+
+T_persistence = 0.050
+
+# Manager properties
 dt = 0.001
-T = 0.005
-time = np.arange(0, T, dt)
-nt = time.size
-time1, time2 = np.meshgrid(time, time)
-cov = np.zeros((nt, nt))
-sigma_in = 1.0
-cov = covariance(time1, time2, tau_s, sigma_in)
+values_to_save = ['o']
+
+
+# Neural Network
+nn = Network(hypercolumns, minicolumns, G=G, tau_s=tau_s, tau_z_pre=tau_z_pre, tau_z_post=tau_z_post,
+             tau_a=tau_a, g_a=g_a, g_I=g_I, sigma_out=sigma_out, epsilon=epsilon, prng=np.random,
+             strict_maximum=strict_maximum, perfect=False, normalized_currents=True)
+
+
+# Build the manager
+manager = NetworkManager(nn=nn, dt=dt, values_to_save=values_to_save)
+x = manager.canonical_activity_representation
+to_modify = int(representation_overlap * len(x[0]))
+start_point = int(0.5 * len(x))
+end_point = len(x)
+start_changing = int(0.5 * (end_point - start_point) * (1 - sequence_overlap)) + start_point
+sequence_overlap_size = int((end_point - start_point) * sequence_overlap)
+for sequence_index in range(start_changing, start_changing + sequence_overlap_size):
+    pattern = x[sequence_index]
+    pattern[:to_modify] = manager.canonical_activity_representation[sequence_index - start_point][:to_modify]
+
+
+
+
