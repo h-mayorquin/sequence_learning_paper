Modified wrong scaling in the noise

play.py
@@ -26,10 +26,10 @@ tau_z_pre = 0.025
 tau_z_post = 0.005
 
 hypercolumns = 10
-minicolumns = 40
+minicolumns = 20
 n_patterns = minicolumns
-representation_overlap = 1.0
-sequence_overlap = 0.5
+representation_overlap = 0.0
+sequence_overlap = 1.0
 
 
 # Training protocol
@@ -51,19 +51,37 @@ nn = Network(hypercolumns, minicolumns, G=G, tau_s=tau_s, tau_z_pre=tau_z_pre, t
              tau_a=tau_a, g_a=g_a, g_I=g_I, sigma_out=sigma_out, epsilon=epsilon, prng=np.random,
              strict_maximum=strict_maximum, perfect=False, normalized_currents=True)
 
-
 # Build the manager
 manager = NetworkManager(nn=nn, dt=dt, values_to_save=values_to_save)
-x = manager.canonical_activity_representation
-to_modify = int(representation_overlap * len(x[0]))
-start_point = int(0.5 * len(x))
-end_point = len(x)
-start_changing = int(0.5 * (end_point - start_point) * (1 - sequence_overlap)) + start_point
-sequence_overlap_size = int((end_point - start_point) * sequence_overlap)
-for sequence_index in range(start_changing, start_changing + sequence_overlap_size):
-    pattern = x[sequence_index]
-    pattern[:to_modify] = manager.canonical_activity_representation[sequence_index - start_point][:to_modify]
+# Build the manager
 
 
+from copy import deepcopy
+
+print('p', representation_overlap)
+print('q', sequence_overlap)
+print('minicolumns', minicolumns)
+print('hypercolumns', hypercolumns)
 
 
+def create_overalaped_representation2(manager, representation_overlap, sequence_overlap):
+    x = deepcopy(manager.canonical_activity_representation)
+
+    to_modify = int(representation_overlap * len(x[0]))
+    sequence_size = int(0.5 * len(x))
+    sequence_overlap_size = int(sequence_overlap * sequence_size)
+    start_point = int(0.5 * sequence_size + sequence_size - np.floor(sequence_overlap_size/ 2.0))
+    end_point = start_point + sequence_overlap_size
+
+    print('middle', int(0.5 * sequence_size) + sequence_size)
+    print('sequence size', sequence_size)
+    print('size to change sequence', sequence_overlap_size)
+    print('start', 'end', start_point, end_point)
+    for sequence_index in range(start_point, end_point):
+        pattern = x[sequence_index]
+        pattern[:to_modify] = manager.canonical_activity_representation[sequence_index - start_point][:to_modify]
+
+    return x
+
+activity = create_overalaped_representation2(manager, representation_overlap, sequence_overlap)
+print(activity)